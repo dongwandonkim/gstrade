9a9366b107df526295cdba7c572632a67235f4de
Dongwan Kim <dongwan.don.kim@gmail.com>
2020-12-02 00:20:00 -0800

    user access restriction & minor item-list work

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
+  #before_action :authenticate_admin_user!
+  #activeadmin
+  before_action :authenticate_user!, except: [:show, :index]
+
   def index
     @items = Item.all
   end
@@ -16,5 +20,29 @@ class ItemsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(params.require(:item).permit(:name, :description, :image_tag))
+      flash[:notice] = "아이템 내용이 수정 되었습니다"
+      redirect_to @item
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   has_many :registered_items
+  has_many :users, through: :registered_items
 end
