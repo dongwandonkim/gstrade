96f613e5fdd07d2eba37305d7867df8a9f5f08ff
Dongwan Kim <dongwan.don.kim@gmail.com>
2020-12-04 20:57:22 -0800

    autocomplete search form for item-search

--- app/javascript/packs/search.js
+++ app/javascript/packs/search.js
@@ -1,28 +1,46 @@
 $(document).on('turbolinks:load', function () {
-  $('#city').autocomplete({
-    source: function (request, response) {
-      $.getJSON('/items.json?term=' + request.term, function (data) {
-        response(
-          $.map(data, function (value, key) {
-            return {
-              label: value.name,
-              value: value.id,
-            };
-          })
-        );
-      });
-    },
-
-    select: function (event, ui) {
-      console.log(ui.item.value);
-      //set value to item id
-      var eee = ui.item.value;
-    },
-
-    focus: function (event, ui) {
+  $('.item-search')
+    .autocomplete({
+      // source: function (request, response) {
+      //   //console.log(request);
+      //   $.getJSON('/items.json', function (data) {
+      //     //console.log(data);
+      //     response(
+      //       $.map(data, function (item) {
+      //         return {
+      //           label: item.name,
+      //           value: item.name,
+      //         };
+      //       })
+      //     );
+      //   });
+      // },
+      source: function (request, response) {
+        $.getJSON('/items.json', function (data) {
+          response(
+            $.map(data, function (obj, key) {
+              var name = obj.name.toUpperCase();
+              //console.log(obj);
+              //검색하려는 단어의 위치가 -1이 같지않다면, (-1은 데이터가없다는 소리)
+              if (name.indexOf(request.term.toUpperCase()) != -1) {
+                return {
+                  label: obj.name, // Label for Display
+                  value: obj.name, // Value
+                  setData: obj.id,
+                };
+              } else {
+                return null;
+              }
+            })
+          );
+        });
+      },
+      //검색하기위한 최소 단어 입력
+      minLength: 1,
+    })
+    .focus(function () {
+      //$(this).autocomplete('search', $(this).val());
       return false;
-
       //event.preventDefault();
-    },
-  });
+    });
 });
