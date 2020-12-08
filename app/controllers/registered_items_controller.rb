53dfa029cfc9b387ce84d0fa4421afee629cfb4a
Dongwan Kim <dongwan.don.kim@gmail.com>
2020-12-08 02:25:34 -0800

    hidden fields basic layouts

--- app/controllers/registered_items_controller.rb
+++ app/controllers/registered_items_controller.rb
@@ -1,10 +1,13 @@
 class RegisteredItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
 
   def index
     @registered_items = RegisteredItem.all
   end
   def new
     @registered_item = RegisteredItem.new
+    @inGameid = current_user.in_game_id
+  
   end
   # item_id: 1, user_id: 8, server_id: 1, category_id: 1, quantity: 3, created_at: "2020-12-03 05:56:16", updated_at: "2020-12-03 05:56:16", price:
   def create
@@ -12,11 +15,9 @@ class RegisteredItemsController < ApplicationController
                                           :item_id, :user_id, :server_id, 
                                           :category_id, :quantity, :price)
                                          )
-    @registered_item.user_id = current_user.id
-    
     # @registered_item = current_user.registered_items.build(registered_items_params)
-    
-    
+    @registered_item.user_id = current_user.id
+
     if @registered_item.save
       flash[:notice] = "등록되었습니다."
       redirect_to @registered_item
@@ -27,6 +28,10 @@ class RegisteredItemsController < ApplicationController
 
   def search
     
+  end
+
+  def show
+    @registered_item = RegisteredItem.find(params[:id])
   end
 
   private
