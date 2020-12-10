974118a5f25935d6ef60cde1843cc2817364ee0e
Dongwan Kim <dongwan.don.kim@gmail.com>
2020-12-09 20:57:15 -0800

    search bar css

--- app/javascript/packs/stickyBar.js
+++ app/javascript/packs/stickyBar.js
@@ -1,18 +1,19 @@
 document.addEventListener('turbolinks:load', function () {
   // When the event DOMContentLoaded occurs, it is safe to access the DOM
 
-  // When the user scrolls the page, execute myFunction
+  // When the user scrolls the page, execute the Function
   window.addEventListener('scroll', stickyBar);
 
   // Get the navbar
-  var searchbar = document.getElementById('sticky-search');
+  var searchbar = document.querySelector('#sticky-search');
   console.log(searchbar);
   //Get the offset position of the navbar
   var sticky = searchbar.offsetTop;
 
   // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
   function stickyBar() {
-    if (window.pageYOffset >= sticky) {
+    if (window.pageYOffset > sticky) {
+      //console.log(window.pageYOffset);
       searchbar.classList.add('sticky');
     } else {
       searchbar.classList.remove('sticky');
