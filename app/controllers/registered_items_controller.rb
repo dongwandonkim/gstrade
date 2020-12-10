7243eac7ed127bc4fb88e20111efbca81a8c3f90
Ediyu <peterhjhan@gmail.com>
2020-12-09 22:13:03 -0800

    code review for registred item

--- app/controllers/registered_items_controller.rb
+++ app/controllers/registered_items_controller.rb
@@ -7,14 +7,11 @@ class RegisteredItemsController < ApplicationController
   def new
     @registered_item = RegisteredItem.new
     @inGameid = current_user.in_game_id
-  
+
   end
   # item_id: 1, user_id: 8, server_id: 1, category_id: 1, quantity: 3, created_at: "2020-12-03 05:56:16", updated_at: "2020-12-03 05:56:16", price:
   def create
-    @registered_item = RegisteredItem.new(params.require(:registered_item).permit(
-                                          :item_id, :user_id, :server_id, 
-                                          :category_id, :quantity, :price)
-                                         )
+    @registered_item = RegisteredItem.new(registered_items_params)
     # @registered_item = current_user.registered_items.build(registered_items_params)
     @registered_item.user_id = current_user.id
 
@@ -27,7 +24,7 @@ class RegisteredItemsController < ApplicationController
   end
 
   def search
-    
+
   end
 
   def show
