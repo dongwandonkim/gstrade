f2577aa4f1015a095aacc18a347bc3ba138ab35e
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-01-08 16:55:35 -0800

    db:setup

--- db/migrate/20201211101208_change_buy_sell_type_to_boolean_in_registered_items.rb
+++ db/migrate/20201211101208_change_buy_sell_type_to_boolean_in_registered_items.rb
@@ -1,5 +1,7 @@
 class ChangeBuySellTypeToBooleanInRegisteredItems < ActiveRecord::Migration[6.0]
   def change
-    change_column :registered_items, :buy_sell, :boolean
+    remove_column :registered_items, :buy_sell, :boolean
+    add_column :registered_items, :buy_sell, :boolean
   end
+  
 end
--- db/migrate/20210108074522_add_casting_to_buy_sell_boolean_column.rb
+++
@@ -1,7 +0,0 @@
-class AddCastingToBuySellBooleanColumn < ActiveRecord::Migration[6.0]
-  def change
-    
-      change_column :registered_items, :buy_sell, 'boolean USING CAST(buy_sell AS boolean)'
-    
-  end
-end
