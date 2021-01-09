ebcd732138e7ac8af9e45bcc44f555a6d01a3206
Dongwan Kim <dongwan.don.kim@gmail.com>
2021-01-08 17:36:17 -0800

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+server = ['백호', '주작', '현무', '청룡', '봉황', '해태', '세종', '신구', '단군', '비호', '태극', '화랑', '태왕']
+
+server.each do |s|
+  Server.create_or_find_by(server_name: s)
+end
